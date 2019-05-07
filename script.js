c7be5f767d2b865793a72f50a8ffe67317fef5a3
friedbergjon <friedberg_jon@hotmail.com>
2019-05-07 12:03:23 -0400

    images loading on screen

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+window.onload = function() {
+
+  const body = document.body;
 
 
+let div = document.createElement('div');
+  div.className ='frog';
+  body.appendChild(div);
+     div.style.left = (0.5 * window.innerWidth) + "px";
+     div.style.top = (0.7 * window.innerHeight) + "px";
 
 
+let div2 = document.createElement('div');
+div2.className = 'car';
+body.appendChild(div2);
+div2.style.left = (0.5 * window.innerWidth) + "px";
+div2.style.top = (0.5 * window.innerHeight) + "px";
 
-
-let frog = document.createElement('img');
-  Image.className='frog';
-  body.appendChild(frog);
+}
