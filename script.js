5c161377f1ea3fb4c5d99f603e3f3322666e3e06
friedbergjon <friedberg_jon@hotmail.com>
2019-05-08 14:50:13 -0400

    frog coordinates changing in console

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
 
 
   const body = document.body;
-const frogPosition = {x: 1, y: 1}
-
+let frogPosition = {x: 7, y: 6}
+let div2Position = {x:8, y:3.5}
+let div3Position = {x:8, y:1.3}
+let div4Position = {x:8, y:5.6}
  
+// function deadFrog() {
   
+// }
+
+
 let frog = document.createElement('div');
   frog.className ='frog';
   body.appendChild(frog);
      frog.style.left = (frogPosition.x * 100) + "px";
      frog.style.top = (frogPosition.y * 100) + "px";
+
     
 
 let div2 = document.createElement('div');
 div2.className = 'car';
 body.appendChild(div2);
-div2.style.left = (0.6  * window.innerWidth) + "px";
-div2.style.top = (0.5 * window.innerHeight) + "px";
+div2.style.left = (div2Position.x * 100) + "px";
+div2.style.top = (div2Position.y * 100) + "px";
 
 
 let div3 = document.createElement('div');
 div3.className = 'car';
 body.appendChild(div3);
-div3.style.left = (0.6 * window.innerWidth) + "px";
-div3.style.top = (0.2 * window.innerHeight) + "px";
+div3.style.left = (div3Position.x * 100) + "px";
+div3.style.top = (div3Position.y * 100) + "px";
 
 let div4 = document.createElement('div');
 div4.className = 'car';
 body.appendChild(div4);
-div4.style.left = (0.6 * window.innerWidth) + "px";
-div4.style.top = (0.7 * window.innerHeight) + "px";
+div4.style.left = (div4Position.x * 100) + "px";
+div4.style.top = (div4Position.y * 100) + "px";
 
-
-let xAxis = 800;
-let x1Axis = 800;
-let x2Axis = 1000;
-
-function moveCar(){
-  div2.style.transform = `translateX(${xAxis}px)`;
-  div3.style.transform = `translateX(${x1Axis}px)`;
-  div4.style.transform = `translateX(${x2Axis}px)`;
-}
 
 //blerf code for keyboard movement, went over in class//
 
 const isCoordinateInGrid = (x, y) => {
-  console.log(x*100)
-  console.log(window.innerWidth)
+
   return !(x < 0 || y < 0 || x*100 > window.innerWidth  || y*100 > window.innerHeight );
 
 }
@@ -60,33 +56,46 @@ const canMoveTo = (x, y) => {
  }
  
  const moveRight = () => {
-     if (canMoveTo(frogPosition.x + 1, frogPosition.y)) {
-         frogPosition.x += 1;
+     if (canMoveTo(frogPosition.x + .5, frogPosition.y)) {
+         frogPosition.x += .5;
+        //  console.log(frogPosition)
+         if (frogPosition.x === div2Position.x ||frogPosition.x === div3Position.x ||frogPosition.x === div4Position.x){
+          xRight = true
+        }
          moveCharacterTo();
      }
  }
  
  const moveLeft = () => {
-     if (canMoveTo(frogPosition.x - 1, frogPosition.y)) {
-         frogPosition.x -= 1;
+     if (canMoveTo(frogPosition.x - .5, frogPosition.y)) {
+         frogPosition.x -= .5;
+         //help from Michael//
+        //  console.log(frogPosition)
+         //if frog coordinates equal car coordinates//
+         if (frogPosition.x === div2Position.x ||frogPosition.x === div3Position.x ||frogPosition.x === div4Position.x){
+          xLeft = true
+         }
          moveCharacterTo();
+        
      }
  }
- 
+ console.log(xLeft)
  const moveDown = () => {
-     if (canMoveTo(frogPosition.x , frogPosition.y + 1)) {
-         frogPosition.y += 1;
+     if (canMoveTo(frogPosition.x , frogPosition.y + .5)) {
+         frogPosition.y += .5;
+         console.log(frogPosition)
          moveCharacterTo();
      }
  }
  
  const moveUp = () => {
-     if (canMoveTo(frogPosition.x , frogPosition.y - 1)) {
-         frogPosition.y -= 1;
+     if (canMoveTo(frogPosition.x , frogPosition.y - .5)) {
+         frogPosition.y -= .5;
+         console.log(frogPosition)
          moveCharacterTo();
      }
  }
- 
+ console.log(frogPosition.y)
  document.body.addEventListener('keydown', evt => {
      const keyCode = evt.keyCode;
   
@@ -116,23 +125,46 @@ const canMoveTo = (x, y) => {
   charElement.style.top = `${(frogPosition.y * 100)}px`;
  }
 
- 
+
+let xAxisCar1 = 800;
+let xAxisCar2 = 800;
+let xAxisCar3 = 1000;
+
+// let yAxisCar1 = 500;
+// let yAxisCar2 = 400;
+// let yAxisCar3 = 300;
+
+
+function moveCar(){
+  div2.style.transform = `translateX(${xAxisCar1}px)`;
+  div3.style.transform = `translateX(${xAxisCar2}px)`;
+  div4.style.transform = `translateX(${xAxisCar3}px)`;
+}
+
 ///Mohammed helped with getting the cars to loop//
 
 setInterval(()=>{
-  x2Axis-= 8;
-  x1Axis-=10;
-  xAxis-=5;
+  xAxisCar1-= 8;
+  xAxisCar2-=10;
+  xAxisCar3-=5;
+  
+  // yAxisCar1-=0.1;
+  // yAxisCar2-=0.1;
+  // yAxisCar3-=0.1;
   moveCar();
 },10)
 
 setInterval(()=>{
-  x2Axis = 900;
-  x1Axis = 1000;
-  xAxis = 800;
+  xAxisCar1 = 900;
+  xAxisCar2 = 1000;
+  xAxisCar3 = 800;
+ 
+  // yAxisCar1 = 500;
+  // yAxisCar2 =400;
+  // yAxisCar3 =300;
   moveCar();
 },5000)
 
-
+// collision detection
 
 
